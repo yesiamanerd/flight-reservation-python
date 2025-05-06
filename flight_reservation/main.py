60dddfdbfd31774911216ca97789ad61461b1d22
Geoffrey Brophy <geoffrey.brophy@proton.me>
2025-05-06 16:30:50 -0400

    adding inital data

---
+++ flight_reservation/main.py
@@ -0,0 +1,15 @@
+# Add the project root directory to sys.path
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+# Task 2, 5, 9: Import modules here
+
+
+# Task 2, 6, 10, 18: Redefine the main() function
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
